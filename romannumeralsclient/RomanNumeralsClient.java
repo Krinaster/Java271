1813507230d40c591683f66f08c25bdaf5eaa391
Krinaster <45246669+Krinaster@users.noreply.github.com>
2023-01-18 23:27:09 -0600

    Update RomanNumeralsClient.java

--- romannumeralsclient/RomanNumeralsClient.java
+++ romannumeralsclient/RomanNumeralsClient.java
@@ -7,15 +7,18 @@ public class RomanNumeralsClient {
     
     public static void main(String[] args) {
         new RomanNumeral();
-        new RomanNumeral(5);
-        try{
+        RomanNumeral test = new RomanNumeral(5);
+        /*try{
             new RomanNumeral(0);
         }
         catch(Exception e){
             System.out.println("Please input a value number");
         }
+       */
         
-
+        test.print();
+        RomanNumeral stringTest = new RomanNumeral("DCLXI");
+        stringTest.print();
     }
     
 }
