023971ee824af1c7a95b84b7f1060a793f3937d7
Krinaster <45246669+Krinaster@users.noreply.github.com>
2023-01-19 09:32:07 -0600

    Update RomanNumeral.java

--- romannumeralsclient/RomanNumeral.java
+++ romannumeralsclient/RomanNumeral.java
@@ -51,6 +51,21 @@ public class RomanNumeral {
         }
         return curValue;
     }
+
+    @Override
+    public String toString(){
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romanLetters = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder roman = new StringBuilder();
+        for(int i=0;i<values.length;i++){ 
+            while(value >= values[i]){
+                value = value - values[i];
+                roman.append(romanLetters[i]);
+                } 
+            }
+
+        return roman.toString();    
+    }
     
     public void setRomanNumeral(int i){
     
