37fb8b6313b8eb5fb8a7cd4a5610a0ca8c6dfb49
Krinaster <45246669+Krinaster@users.noreply.github.com>
2023-01-23 09:45:54 -0600

    Update RomanNumeralsClient.java

--- romannumeralsclient/RomanNumeralsClient.java
+++ romannumeralsclient/RomanNumeralsClient.java
@@ -1,25 +1,27 @@
-
 package romannumeralsclient;
+
+import java.util.Random;
 
 
 public class RomanNumeralsClient {
 
     
     public static void main(String[] args) {
-        new RomanNumeral();
-        RomanNumeral test = new RomanNumeral(4);
-        /*try{
-            new RomanNumeral(0);
-        }
-        catch(Exception e){
-            System.out.println("Please input a value number");
-        }
-       */
         
-        test.print();
-        RomanNumeral stringTest = new RomanNumeral("DCLXI");
-        stringTest.print();
-        System.out.println(test.toString());
+        Random rand = new Random();
+        RomanNumeral[] r = new RomanNumeral[10];
+        for(int i =1; i<r.length; i++)
+            r[i] = new RomanNumeral(rand.nextInt(4000) + 1);
+        r[0]  = new RomanNumeral();
+        
+        for(RomanNumeral nr: r)
+            System.out.println(nr.getValue() + " = " + nr);
+        
+        RomanNumeral test1 = new RomanNumeral(5);
+        RomanNumeral test2 = new RomanNumeral("V");
+        
+        System.out.println(test1.equals(test2));
+        
     }
     
 }
