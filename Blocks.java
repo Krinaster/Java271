016abb10039d1d8f4dd0f593e120dbffb598625a
Krinaster <45246669+Krinaster@users.noreply.github.com>
2023-04-23 17:45:35 -0500

    Update Blocks.java

--- Blocks.java
+++ Blocks.java
@@ -1,6 +1,6 @@
+package Blocks;
 
-package blocks;
-
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Stack;
@@ -11,7 +11,7 @@ public class Blocks {
     public static Stack<Integer>[] blocks;
     public static Stack<Integer> temp;
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         /*Scanner stdin = new Scanner(System.in);
 		int numOfBlocks = stdin.nextInt();
@@ -21,45 +21,50 @@ public class Blocks {
 		System.out.println(input);
     */
 
-	Scanner stdin = new Scanner(System.in);
-	int numOfBlocks = stdin.nextInt();
-	stdin.nextLine();
-	blocks = new Stack[numOfBlocks];
+	    // Scanner stdin = new Scanner(System.in);
+        Scanner stdin = new Scanner(new File("blocks2.txt"));
+	    int numOfBlocks = stdin.nextInt();
+	    stdin.nextLine();
+	    
+        //Stack<Integer>[] blocks = new Stack[numOfBlocks];
+        blocks = new Stack[numOfBlocks];
        	temp = new Stack<>();
         pos = new int[numOfBlocks];
-    	String command = null, keyword = null;
+    	
+
+        String command = null, keyword = null;
     	int num1 = 0, num2 = 0;
 
     	// Initializing initial positions of position array and block location
-	for(int i =0; i<blocks.length; i++){
-		blocks[i] = new Stack<>();
-		blocks[i].push(i);
-		pos[i] = i;
-	}
+	    for(int i =0; i<blocks.length; i++){
+		    blocks[i] = new Stack<>();
+		    blocks[i].push(i);
+		    pos[i] = i;
+	    }
         
 	
-	while(!stdin.hasNext("quit")){
-		String s = stdin.nextLine();
-		String[] splinter = s.split(" ", 0);
-		command = splinter[0].trim();
-		keyword = splinter[2].trim();
-		num1 = Integer.parseInt(splinter[1]);
-		num2 = Integer.parseInt(splinter[3]);
+	    while(!stdin.hasNext("quit")){
+		    String s = stdin.nextLine();
+		    String[] splinter = s.split(" ", 0);
+		    command = splinter[0].trim();
+		    keyword = splinter[2].trim();
+		    num1 = Integer.parseInt(splinter[1]);
+		    num2 = Integer.parseInt(splinter[3]);
 
-		if(command.equals("move")){
+		    if(command.equals("move")){
                     if(keyword.equals("onto"))
-			moveOnto(num1, num2);
+			    moveOnto(num1, num2);
                     else
-			moveOver(num1, num2);
-		}
-		else if(command.equals("pile")){
+			    moveOver(num1, num2);
+		    }
+		    else if(command.equals("pile")){
                     if(keyword.equals("onto"))
-			pileOnto(num1, num2);
+			    pileOnto(num1, num2);
                     else
-			pileOver(num1, num2);
-		}
-		else 
-                    throw new Error("You messed up");
+			    pileOver(num1, num2);
+		    }
+		    else 
+                throw new Error("You messed up");
             }
             
             printBlocks();
@@ -69,42 +74,36 @@ public class Blocks {
 	} // End of Main
 
 	public static void moveOnto(int num1, int num2){
-		// First return all the blocks ontop of a and b
-		int positionA = blocks[pos[num1]].indexOf(num1);
-                int positionB = blocks[pos[num2]].indexOf(num2);
-                
-                System.out.println("blocks size " + blocks[num1].size() );
+		        // First return all the blocks ontop of a and b
                 // Cycling through array after index of num1
-                System.out.println("index of " + blocks[pos[num1]].indexOf(num1));
-                System.out.println("size of " + blocks[pos[num1]].size());
-                System.out.println("index of pt 2" + blocks[pos[num1]].indexOf(positionA));
-                while(blocks[pos[num1]].indexOf(num1) < blocks[pos[num1]].size()){
-                    moveBack(blocks[pos[num1]].indexOf(++positionA));
+				while(blocks[pos[num1]].peek() != num1){
+                    moveBack(blocks[pos[num1]].pop());
                 }
-                
+
                 // Cycling through array after index of num2
-                while(blocks[pos[num2]].indexOf(num2) < blocks[pos[num2]].size()){
-                    moveBack(blocks[pos[num1]].indexOf(++positionB));
+                while(blocks[pos[num2]].peek() != num2){
+                    moveBack(blocks[pos[num2]].pop());
                 }
                 
                 // Pushing num1 onto num2 stack, removing it from the original stack 
                 // and setting position to block 2 stack
-		blocks[pos[num2]].push(num1);
-                blocks[pos[num1]].remove(num1);
+				blocks[pos[num2]].push(blocks[pos[num1]].pop());
                 pos[num1] = pos[num2];
+            
 	}
 
+    // Look at line 3 of the data file for this is moving block 7 back
+    // when it should not
 	public static void moveOver(int num1, int num2){
-            // First return all block ontop of a
-            int positionA = blocks[pos[num1]].indexOf(num1);
-            
+            // First return all block ontop of a;
             // Cycling through array after index of num1
-            while(blocks[pos[num1]].indexOf(num1) < blocks[pos[num1]].size()){
-                moveBack(blocks[pos[num1]].indexOf(++positionA));
+            if(pos[num1] != pos[num2]){
+                while(blocks[pos[num1]].peek() != num1){
+				    moveBack(blocks[pos[num1]].pop());
+			    }
             }
-            
-            blocks[pos[num2]].push(num1);
-            blocks[pos[num1]].remove(num1);
+            // Pushing block a onto stack b and updating position of element a
+            blocks[pos[num2]].push(blocks[pos[num1]].pop());
             pos[num1] = pos[num2];
             
 	}
@@ -112,20 +111,19 @@ public class Blocks {
 	public static void pileOnto(int num1, int num2){
             
             // Pushing every element ontop of the stack until we reach a
-            for(int i =blocks[pos[num1]].size(); i>blocks[pos[num1]].indexOf(num1); i--){
+            while(blocks[pos[num1]].peek() != num1){
                 temp.push(blocks[pos[num1]].pop());
             }
-
-            // First add all blocks and a onto temporary stack
-            int positionB = blocks[pos[num2]].indexOf(num2);
+            // Pushing element a
+            temp.push(blocks[pos[num1]].pop());
             
             // Returning all blocks on top of B
-            while(blocks[pos[num2]].indexOf(num2) < blocks[pos[num2]].size()){
-                moveBack(blocks[pos[num1]].indexOf(++positionB));
-            }
+            while(blocks[pos[num2]].peek() != num2){
+				moveBack(blocks[pos[num2]].pop());
+			}
             
             // Adding temp stack to b
-            while(temp.size() > 0){
+            while(!temp.isEmpty()){
                 pos[temp.peek()] = pos[num2];
                 blocks[pos[num2]].push(temp.pop());
             }
@@ -134,15 +132,18 @@ public class Blocks {
 
 	public static void pileOver(int num1, int num2){
             // Pushing every element ontop of the stack until we reach a
-            for(int i =blocks[pos[num1]].size(); i>blocks[pos[num1]].indexOf(num1); i--){
+            while(blocks[pos[num1]].peek() != num1){
                 temp.push(blocks[pos[num1]].pop());
             }
+            
+            // Pushing element a on the temp stack
+            temp.push(blocks[pos[num1]].pop());
             
             // Adding temp stack to b
             while(temp.size() > 0){
                 pos[temp.peek()] = pos[num2];
                 blocks[pos[num2]].push(temp.pop());
-            }// Adding temp stack to b
+            }
             
 	}
         
@@ -150,21 +151,29 @@ public class Blocks {
         // Method to call to move numbers back to original block location
         public static void moveBack(int num){
             //pos[num] = num;
-            System.out.println("blocks size " + blocks[num].size());
-            //blocks[num].push(blocks[pos[num]].pop());
-            //pos[num] = num;
+            // System.out.println("blocks size " + blocks[num].size());
+            blocks[num].push(num);
+            pos[num] = num;
         }
         
         public static void printBlocks(){
-            for(int i =0; i<blocks.length; i++){
-                System.out.println(i + ": ");
-                for(int j=0; j<blocks[i].size(); j++){
-                    System.out.print(blocks[i].pop() + " ");
+            // Pushing them onto temp then printing
+			for(int i=0; i<blocks.length; i++){
+				System.out.print(i + ": ");
+				
+                while(!blocks[i].isEmpty()){
+					temp.push(blocks[i].pop());
                 }
-                
-            }
+
+				while(!temp.isEmpty()){
+					System.out.print(temp.pop() + " ");
+                }
+
+				System.out.println();
+			}
+
+
         
         }
 
 } // End of class Blocks
-
